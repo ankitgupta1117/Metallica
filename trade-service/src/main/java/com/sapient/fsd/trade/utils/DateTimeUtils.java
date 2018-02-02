8c1180b6e6d28a9ce5c31cf58712c161d074bb4b
Ankit gupta <ankitgupta1117@gmail.com>
2018-02-03 00:47:31 +0530

    Added DateTime utils for date conversion

--- trade-service/src/main/java/com/sapient/fsd/trade/utils/DateTimeUtils.java
+++ trade-service/src/main/java/com/sapient/fsd/trade/utils/DateTimeUtils.java
@@ -9,7 +9,7 @@ import java.time.format.DateTimeFormatter;
  */
 public class DateTimeUtils {
 
-    private static DateTimeFormatter DATE_TIME_FORMATTER  = DateTimeFormatter.ofPattern("DD-MM-uuuu");
+    private static DateTimeFormatter DATE_TIME_FORMATTER  = DateTimeFormatter.ofPattern("dd-MM-uuuu");
     public static LocalDateTime parseDate(String dateStr){
         return LocalDateTime.from(LocalDate.parse(dateStr,DATE_TIME_FORMATTER).atStartOfDay());
 
@@ -18,5 +18,10 @@ public class DateTimeUtils {
     public static String formatDate(LocalDateTime date){
         return DATE_TIME_FORMATTER.format(date);
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LocalDate.parse("02-02-2018",DATE_TIME_FORMATTER));
+        DateTimeUtils.parseDate("02-02-2018");
     }
 }
