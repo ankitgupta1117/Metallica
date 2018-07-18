7449457ca8daa31a5489b612a5bf1cf4297ab0d3
Ankit gupta <ankitgupta1117@gmail.com>
2018-07-18 12:28:48 +0530

    Fixed failing Junits

--- ref-service/src/test/java/com/sapient/fsd/refdata/controllers/CommodityControllerTest.java
+++ ref-service/src/test/java/com/sapient/fsd/refdata/controllers/CommodityControllerTest.java
@@ -2,10 +2,13 @@ package com.sapient.fsd.refdata.controllers;
 
 import com.sapient.fsd.common.models.Commodity;
 import com.sapient.fsd.refdata.services.CommodityRefService;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,6 +18,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 public class CommodityControllerTest {
 
@@ -22,18 +29,35 @@ public class CommodityControllerTest {
 
     @InjectMocks
     private CommodityController commodityController;
-    @InjectMocks
+    @Mock
     private CommodityRefService commodityRefService;
+
     @Before
-    public void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(commodityController, commodityRefService).
-                build();
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(commodityController).build();
     }
 
     @Test
-    public void listCommoditiesTest() throws Exception {
+    public void listAllCommoditiesTest() throws Exception {
+        Commodity zn = new Commodity("ZN", "Zinc");
+        Commodity ag= new Commodity("AG", "Silver");
+        Commodity au = new Commodity("AU", "Gold");
+        Commodity cu = new Commodity("CU", "Copper");
+
+        when(commodityRefService.getAllCommodities()).thenReturn(Arrays.asList(ag,au,cu,zn));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/commodity/list"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void searchByCodeTest() throws Exception {
+        Commodity zn = new Commodity("ZN", "Zinc");
+
+        when(commodityRefService.findByCode("ZN")).thenReturn(zn);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/commodity/search?code=zn"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
